8e9867768b4df40e256749f3cdfe4fbbb457e742
Seyitan499 <seyitan.fashola@gmail.com>
2026-01-04 03:37:09 +0100

    sixth commit

--- backend/app/telegram.py
+++ backend/app/telegram.py
@@ -23,26 +23,52 @@
 #     print("📨 Telegram task triggered")
 #     print(text)
 
+# import requests
+# import time
+# from app.config import TELEGRAM_BOT_TOKEN, TELEGRAM_CHAT_ID
+
+
+# def send_telegram_message(text: str) -> None:
+#     time.sleep(0.2)
+
+#     url = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage"
+
+#     payload = {
+#         "chat_id": TELEGRAM_CHAT_ID,
+#         "text": text,
+#         "parse_mode": "HTML",
+#     }
+
+#     try:
+#         response = requests.post(url, json=payload, timeout=5)
+#         print("Telegram status:", response.status_code)
+#         print("Telegram response:", response.text)
+        
+#     except Exception as e:
+#         print("Telegram exception:", e)
+
 import requests
-import time
+import threading
 from app.config import TELEGRAM_BOT_TOKEN, TELEGRAM_CHAT_ID
 
 
-def send_telegram_message(text: str) -> None:
-    time.sleep(0.2)
-
-    url = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage"
-
-    payload = {
-        "chat_id": TELEGRAM_CHAT_ID,
-        "text": text,
-        "parse_mode": "HTML",
-    }
-
+def _send(text: str):
     try:
-        response = requests.post(url, json=payload, timeout=5)
-        print("Telegram status:", response.status_code)
-        print("Telegram response:", response.text)
-        
+        requests.post(
+            f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage",
+            json={
+                "chat_id": TELEGRAM_CHAT_ID,
+                "text": text,
+                "parse_mode": "HTML",
+            },
+            timeout=5,
+        )
     except Exception as e:
         print("Telegram exception:", e)
+
+
+def send_telegram_message(text: str):
+    thread = threading.Thread(target=_send, args=(text,))
+    thread.daemon = True
+    thread.start()
+
